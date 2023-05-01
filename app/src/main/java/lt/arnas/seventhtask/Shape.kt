12c7419f8ada1ae78ebef443769b3e98412aa8a5
AKasparavicius <kasparavicius.ar@gmail.com>
2023-05-01 23:56:19 +0300

    Add Shape.kt

---
+++ app/src/main/java/lt/arnas/seventhtask/Shape.kt
@@ -0,0 +1,15 @@
+package lt.arnas.seventhtask
+
+class Shape(var sideOne: Int,
+            var sideTwo: Int) {
+
+    var area: Int = 0
+
+    fun calculateArea(){
+        area = sideOne * sideTwo
+    }
+
+    override fun toString(): String {
+        return "Jusu figuros plotas yra $area kv.cm"
+    }
+}
