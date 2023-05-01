32c8f5399f52b90f50dc922aca941a295282cf68
AKasparavicius <kasparavicius.ar@gmail.com>
2023-05-02 00:24:26 +0300

    Add fun chooseShape to MainActivity.kt

--- app/src/main/java/lt/arnas/seventhtask/MainActivity.kt
+++ app/src/main/java/lt/arnas/seventhtask/MainActivity.kt
@@ -2,6 +2,7 @@ package lt.arnas.seventhtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val square01 = Square(5)
+        val triangle01 = Triangle(8, 10)
+        val circle01 = Circle(4)
+
+        chooseShape(square01)
+        chooseShape(triangle01)
+        chooseShape(circle01)
     }
+
+    private fun chooseShape(shape: Shape) {
+        shape.calculateArea()
+        Log.i(TAG, "chooseShape: $shape")
+    }
+
+
 }
